64c7c34c721cf11e4e9845c3a908966af31ec653
“pankaj <panbhatt@gmail.com>
2023-07-20 11:35:20 -0400

    Adding the initial skeleton of an Golang project

---
+++ initializers/loadEnv.go
@@ -0,0 +1,30 @@
+package initializers
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DBHhost    string `mapstructure:"POSTGRES_HOST"`
+	DBUser     string `mapstructure:"POSTGRES_USER"`
+	DBPassword string `mapstructure:"POSTGRES_PASSWORD"`
+	DBName     string `mapstructure:"POSTGRES_DB"`
+	DBPort     string `mapstructure:"POSTGRES_PORT"`
+
+	ServerPort   string `mapstructure:"PORT"`
+	ClientOrigin string `mapstructure:"CLIENT_ORIGIN"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("app")
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+
+}
